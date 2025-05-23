16f5cde105c0fb6944a1f24021de95dccc32d229
Muhammad Faisal <faisalazam_1616@hotmail.com>
2025-05-23 18:31:46 +1000

    Creating and mounting the ./secrets/jcasc_secrets dir.

--- docker/jenkins/scripts/bootstrap.sh
+++ docker/jenkins/scripts/bootstrap.sh
@@ -1,32 +1,43 @@
 #!/bin/sh
 
+# Set fail-fast behavior for the entire script.
+set -e
+
+# This script should be run manually before executing `docker compose up`.
+# It creates the required secret files based on the environment variables.
+#
+# Usage example:
+#   export JENKINS_ADMIN_PASSWORD=Admin@123
+#   export JENKINS_CI_USER_PASSWORD=cI_User@123
+#   ./scripts/bootstrap.sh
+
 # Capture env vars immediately and unset right away — upfront and first thing,
 # so even if the below script fails for whatever reason, the sensitive vars are always unset.
-ADMIN_PASSWORD_VALUE="$ADMIN_PASSWORD"
-unset ADMIN_PASSWORD
+JENKINS_ADMIN_PASSWORD_VALUE="$JENKINS_ADMIN_PASSWORD"
+unset JENKINS_ADMIN_PASSWORD
 
-CI_USER_PASSWORD_VALUE="$CI_USER_PASSWORD"
-unset CI_USER_PASSWORD
+JENKINS_CI_USER_PASSWORD_VALUE="$JENKINS_CI_USER_PASSWORD"
+unset JENKINS_CI_USER_PASSWORD
 
-SECRET_DIR="${JENKINS_JCASC_SECRETS_DIR}"
+SECRET_DIR="./secrets/jcasc_secrets"
 
 mkdir -p "$SECRET_DIR"
 chmod 0700 "$SECRET_DIR"
 
 write_secret() {
   USER_PASSWORD="$1"
-  FILE_NAME="$2"
+  FILE_PATH="$SECRET_DIR/$2"
 
   if [ -n "$USER_PASSWORD" ]; then
-    printf %s "$USER_PASSWORD" | base64 | tr -d '\n' > "$SECRET_DIR/$FILE_NAME"
-    chmod 0600 "$SECRET_DIR/$FILE_NAME"
-    echo "[INFO] Secret has been written to $SECRET_DIR/$FILE_NAME"
+    printf %s "$USER_PASSWORD" | base64 | tr -d '\n' > "$FILE_PATH"
+    chmod 0600 "$FILE_PATH"
+    echo "[INFO] Secret has been written to $FILE_PATH"
   else
-    echo "[WARN] Password is empty. $FILE_NAME not created." >&2
+    echo "[WARN] Password is empty. $FILE_PATH not created." >&2
   fi
 }
 
-write_secret "$ADMIN_PASSWORD_VALUE" "admin_password"
-write_secret "$CI_USER_PASSWORD_VALUE" "ci_user_password"
+write_secret "$JENKINS_ADMIN_PASSWORD_VALUE" "admin_password"
+write_secret "$JENKINS_CI_USER_PASSWORD_VALUE" "ci_user_password"
 
-exec /usr/local/bin/jenkins.sh
+echo "[DONE] Secrets written. Now run: docker compose up"
