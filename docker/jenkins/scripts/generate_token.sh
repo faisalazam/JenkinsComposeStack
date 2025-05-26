26a6bbd86f22d15c9b24b5b5f01d3e52753df43d
Muhammad Faisal <faisalazam_1616@hotmail.com>
2025-05-26 15:24:02 +1000

    Adjusted the generate_token.sh script so that it can be run inside the container instead of the host (to avoid the permission issues while reading the secret files if run from host).

--- docker/jenkins/scripts/generate_token.sh
+++ docker/jenkins/scripts/generate_token.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
+# MSYS_NO_PATHCONV=1 docker exec jenkins sh /usr/local/bin/scripts/generate_token.sh
+
 CI_USER="ci_user"
 TOKEN_NAME="ci_user_api_token"
-COOKIE_JAR="secrets/cookies.txt"
 JENKINS_URL="http://localhost:8080"
-CI_USER_API_TOKEN_FILE="secrets/ci_user_api_token.txt"
-CI_USER_PASSWORD_FILE="secrets/jcasc_secrets/ci_user_password"
+COOKIE_JAR="/var/jenkins_home/jcasc_secrets/generated/cookies"
+CI_USER_PASSWORD_FILE="/var/jenkins_home/jcasc_secrets/ci_user_password"
+CI_USER_API_TOKEN_FILE="/var/jenkins_home/jcasc_secrets/generated/ci_user_api_token"
 CI_USER_PASSWORD="$(base64 -d "$CI_USER_PASSWORD_FILE")"
+
+mkdir -p "/var/jenkins_home/jcasc_secrets/generated"
 
 # Fetch CSRF token
 CRUMB_JSON=$(curl -c "$COOKIE_JAR" -s -u "$CI_USER:$CI_USER_PASSWORD" "$JENKINS_URL/crumbIssuer/api/json")
@@ -25,6 +29,7 @@ TOKEN_VALUE=$(echo "$RESPONSE" | sed -n 's/.*"tokenValue":"\([^"]*\)".*/\1/p')
 
 if [ -n "$TOKEN_VALUE" ]; then
     echo "$TOKEN_VALUE" > "$CI_USER_API_TOKEN_FILE"
+    chmod 600 "$CI_USER_API_TOKEN_FILE"
     echo "✅ API token saved to $CI_USER_API_TOKEN_FILE"
 else
     echo "❌ Failed to extract API token"
